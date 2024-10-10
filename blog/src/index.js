4c4a619ce50ba2b75dcf5b7f3b2b7484b2dbad0b
Laisky.Cai <github@laisky.com>
2024-10-10 06:23:00 +0000

    fix: Update globals package to version 15.11.0 and increment cache prefix to v2.21

--- blog/src/index.js
+++ blog/src/index.js
@@ -20,7 +20,7 @@ Listening on routes:
     * gq.laisky.com/*
 */
 
-setDefaultCachePrefix("blog-v2.20/");
+setDefaultCachePrefix("blog-v2.21/");
 
 const GraphqlAPI = "https://gq.laisky.com/query/";
 
